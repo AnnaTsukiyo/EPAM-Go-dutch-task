e861acb0ac87e420aae9775f5efccfcbb7f1c8ac
AnnaTsukiyo <annett.sav4enko@gmail.com>
2022-01-23 19:39:28 +0200

    done

--- src/main/java/com/epam/rd/autotasks/godutch/GoDutch.java
+++ src/main/java/com/epam/rd/autotasks/godutch/GoDutch.java
@@ -1,8 +1,24 @@
 package com.epam.rd.autotasks.godutch;
 
+import java.util.Scanner;
+
 public class GoDutch {
 
     public static void main(String[] args) {
         //Write code here
+        Scanner scanner = new Scanner(System.in);
+        var billTotal = scanner.nextInt();
+        var friendsAmountTotal = scanner.nextInt();
+        if (billTotal < 0) {
+            System.out.println("Bill total amount cannot be negative");
+        } else if (friendsAmountTotal <= 0) {
+            System.out.println("Number of friends cannot be negative or zero");
+        } else if (billTotal < friendsAmountTotal) {
+            System.out.println(1);
+        } else {
+            int amountPerPerson = billTotal / friendsAmountTotal;
+            int includingFees = (int) (amountPerPerson + (amountPerPerson * 0.1));
+            System.out.println(includingFees);
+        }
     }
 }
